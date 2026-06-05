ffdbe9ffb7b336e3216eb940b37f388e4ed78733
ratara5 <ratara5@gmail.com>
2026-06-05 18:06:10 -0500

    fix: replace Flask initialization syntax with FastAPI

--- app/server.py
+++ app/server.py
@@ -5,9 +5,7 @@ from app.core.settings import settings
 from app.api.routes import all_router
 
 def create_app() -> FastAPI:
-    app = FastAPI(__name__)
-    app.config["JWT_SECRET"] = settings.jwt_secret
-    app.config["MAX_CONTENT_LENGTH"] = 20 * 1024 * 1024
+    app = FastAPI()
     #JWTManager(app)
 
     for router in all_router:
